bf495d0079c682a989b454b3b7aa1da1abe68ea6
PeweezDev <jalinski.dev@gmail.com>
2024-06-09 19:27:46 +0200

    init commit

---
+++ CannaGrowCompanion.UI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CannaGrowCompanion.UI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ CannaGrowCompanion.UI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CannaGrowCompanion.UI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
